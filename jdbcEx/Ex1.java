eddcb7a7c12fdcb8c4a994900f6bec33b4e2e523
Umang Yadav <umang.work82@gmail.com>
2021-06-14 23:14:48 +0530

    commit JDBC CLI App

--- jdbcEx/Ex1.java
+++ jdbcEx/Ex1.java
@@ -1,19 +1,21 @@
- 
 package jdbcEx;
 
-import java.sql.*; 
+import java.sql.*;
+
 public class Ex1 {
-    public static void main(String[] args){
-        try{
+
+    public static void main(String[] args) {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/student","root", "Umang@123");
-            if(conn != null){
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/student", "root", "Umang@123");
+            if (conn != null) {
                 System.out.println("Connection Created..!");
-            }else{ 
+                
+            } else {
                 System.out.println("Connection not Created..!");
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-    } 
+    }
 }
---
+++ jdbcEx/JDBCImplementation.java
@@ -0,0 +1,124 @@
+package jdbcEx;
+
+import java.sql.*;
+import java.util.*;
+import utilities.GetValue;
+class DatabaseConnectivity {
+
+    public static Connection conn;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/student", "root", "Umang@123");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static boolean enterRecord(String name, String address) {
+        try {
+            Statement st = conn.createStatement();
+            boolean c = st.execute("insert into `student`.`student`(name,address) values ('" + name + "','" + address + "');");
+            st.close();
+            return c;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    
+    public static ArrayList<String> getDetails(String name){
+        ArrayList<String> str = null;
+        try{
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from `student`.`student` where name like '%"+name+"%' ;");
+            int i =0;
+            str = new ArrayList<>();
+            while(rs.next()){
+                str.add(rs.getInt("sid")+"\t"+rs.getString("name")+"\t"+rs.getString("address"));
+            }
+                
+        }catch(Exception ex){
+            ex.printStackTrace();
+        } 
+        return str;
+    }
+}
+
+public class JDBCImplementation {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void showWelcomeMenu() {
+        System.out.println("------------------------------");
+        System.out.println("SCHOOL Records:");
+        System.out.println("******Select your choice******");
+        System.out.println("1- Enter Record");
+        System.out.println("2- View Record");
+        System.out.println("3- Update Records");
+        System.out.println("------------------------------");
+        System.out.println("4- Exit");
+        System.out.println("------------------------------");
+    }
+
+    public static void enterRecordsM1() {
+        System.out.println("------------------------------");
+        System.out.println("SCHOOL Records:");
+        System.out.println("******Select your choice******");
+        System.out.println("Enter name:");
+        String name = GetValue.scanString();
+        System.out.println("Enter Address:");
+        String address = GetValue.scanString();
+        DatabaseConnectivity.enterRecord(name, address);
+        System.out.println("------------------------------");
+    }
+
+    public static void viewRecordsM1() {
+        System.out.println("------------------------------");
+        System.out.println("SCHOOL Records:");
+        System.out.println("******Select your choice******"); 
+        String name = GetValue.scanString("Enter name:");
+        System.out.println("Sid\tName\tAddress");
+      ArrayList<String> result = DatabaseConnectivity.getDetails(name);
+        for(String i : result)
+            System.out.println(i);
+        System.out.println("------------------------------");
+        
+        
+    }
+
+    public static void updateRecordsM1() {
+
+    }
+
+    public static void main(String[] args) {
+        char ch = 'n';
+        do {
+            showWelcomeMenu();
+        int c = sc.nextInt();
+            switch(c){
+                case 1:
+                    enterRecordsM1();
+                    break;
+                case 2:
+                    viewRecordsM1();
+                    break;
+                case 3:
+                    updateRecordsM1();
+                    break;
+                case 4:
+                    System.out.println("Exited Succefully..!");
+                    System.exit(0);
+                default:
+                    System.out.println("Wrong choice");
+            }
+               
+                    System.out.println("Do you want to continue(Yes->'y' | No->'n')");
+                    ch = sc.next().charAt(0);
+                
+        } while (ch=='y');
+       
+    }
+
+}
